3ccb22aebeff426de411ff781abcac698a7a1a6b
Chris Parolini <chris.parolini@chatid.com>
2016-11-15 18:24:31 -0600

    pushing files that were added during my solo review

---
+++ class-examples/JoanOfArc/JoanofArcObjects.js
@@ -0,0 +1,17 @@
+// Our joan object.
+var joan = {
+  "Real Name": "Jehanne la Pucelle.",
+  "Grew Up Where": "Domremy, a village in northeastern France.",
+  "Known For": "Peasant girl, daughter of a farmer, who rose to become Commander of the French army.",
+  "Scars": "Took an arrow to the shoulder and a crossbow bolt to the thigh while liberating Paris.",
+  "Symbolism": "Stands for French unity and nationalism."
+};
+
+// We can then access the properties using the below approach:
+joan["Real Name"];
+joan["Grew Up Where"];
+
+
+// If the "property" is only one word -- it's even easier.
+joan.Symbolism;
+joan.Scars;
